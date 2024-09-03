448f3a0bbbfb59ad0e62a5d8d099fd309a1c1a20
happenwah <tiagolixa@hotmail.com>
2024-09-03 18:55:55 +0100

    chore: cleanup + minor fixes (WIP)

--- src/Validly.sol
+++ src/Validly.sol
@@ -12,7 +12,7 @@ import {ISovereignPool} from "@valantis-core/pools/interfaces/ISovereignPool.sol
 
 /**
  * @title Validly Liquidity Module.
- * @dev UniswapV2 style constant product, and Solidly' stable invariant
+ * @dev UniswapV2 style constant product and Solidly's stable invariant,
  *      implemented as a Valantis Sovereign Liquidity Module.
  */
 contract Validly is ISovereignALM, ERC20, ReentrancyGuard {
@@ -39,6 +39,7 @@ contract Validly is ISovereignALM, ERC20, ReentrancyGuard {
     error Validly__withdraw_insufficientToken1Withdrawn();
     error Validly__withdraw_zeroShares();
     error Validly__withdraw_invalidRecipient();
+    error Validly___get_y_notConverged();
 
     /**
      *
@@ -60,17 +61,17 @@ contract Validly is ISovereignALM, ERC20, ReentrancyGuard {
     ISovereignPool public immutable pool;
 
     /**
-     * @dev Boolean indicating if the pool is stable or volatile
+     * @dev Boolean indicating if the pool is stable or volatile.
      */
     bool public immutable isStable;
 
     /**
-     * @dev Decimals of token0
+     * @dev Decimals of token0.
      */
     uint256 public immutable decimals0;
 
     /**
-     * @dev Decimals of token1
+     * @dev Decimals of token1.
      */
     uint256 public immutable decimals1;
 
@@ -79,12 +80,16 @@ contract Validly is ISovereignALM, ERC20, ReentrancyGuard {
      *  CONSTRUCTOR
      *
      */
-    constructor(address _pool, bool _isStable) ERC20("Validly LP Token", "VAL-LP") {
+    constructor(
+        address _pool,
+        bool _isStable
+    ) ERC20("Validly LP Token", "VAL-LP") {
         if (_pool == address(0)) revert Validly__constructor_invalidPool();
 
         pool = ISovereignPool(_pool);
 
-        if (pool.sovereignVault() != _pool) revert Validly__constructor_customSovereignVaultNotAllowed();
+        if (pool.sovereignVault() != _pool)
+            revert Validly__constructor_customSovereignVaultNotAllowed();
 
         isStable = _isStable;
 
@@ -116,7 +121,7 @@ contract Validly is ISovereignALM, ERC20, ReentrancyGuard {
      */
 
     /**
-     * @notice Deposit liquidity into `POOL` and mint LP tokens.
+     * @notice Deposit liquidity into `pool` and mint LP tokens.
      * @param _amount0 Amount of token0 deposited.
      * @param _amount1 Amount of token1 deposited.
      * @param _minShares Minimum amount of shares to mint.
@@ -124,13 +129,20 @@ contract Validly is ISovereignALM, ERC20, ReentrancyGuard {
      * @param _recipient Address to mint LP tokens for.
      * @return shares Amount of shares minted.
      */
-    function deposit(uint256 _amount0, uint256 _amount1, uint256 _minShares, uint256 _deadline, address _recipient)
+    function deposit(
+        uint256 _amount0,
+        uint256 _amount1,
+        uint256 _minShares,
+        uint256 _deadline,
+        address _recipient
+    )
         external
         ensureDeadline(_deadline)
         nonReentrant
         returns (uint256 shares, uint256 amount0, uint256 amount1)
     {
-        if (_recipient == address(0)) revert Validly__deposit_invalidRecipient();
+        if (_recipient == address(0))
+            revert Validly__deposit_invalidRecipient();
 
         uint256 totalSupplyCache = totalSupply();
         if (totalSupplyCache == 0) {
@@ -142,41 +154,57 @@ contract Validly is ISovereignALM, ERC20, ReentrancyGuard {
 
             // _shares param is ignored during first deposit
             shares = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
-            if (shares < _minShares) revert Validly__deposit_lessThanMinShares();
         } else {
             (uint256 reserve0, uint256 reserve1) = pool.getReserves();
 
             uint256 shares0 = Math.mulDiv(_amount0, totalSupplyCache, reserve0);
             uint256 shares1 = Math.mulDiv(_amount1, totalSupplyCache, reserve1);
-
-            if (shares0 < _minShares && shares1 < _minShares) revert Validly__deposit_lessThanMinShares();
-
             // Normal deposits are made using `onDepositLiquidityCallback`
             if (shares0 < shares1) {
                 shares = shares0;
-                amount1 = Math.mulDiv(reserve1, shares, totalSupplyCache, Math.Rounding.Ceil);
+                amount1 = Math.mulDiv(
+                    reserve1,
+                    shares,
+                    totalSupplyCache,
+                    Math.Rounding.Ceil
+                );
                 amount0 = _amount0;
             } else {
                 shares = shares1;
-                amount0 = Math.mulDiv(reserve0, shares, totalSupplyCache, Math.Rounding.Ceil);
+                amount0 = Math.mulDiv(
+                    reserve0,
+                    shares,
+                    totalSupplyCache,
+                    Math.Rounding.Ceil
+                );
                 amount1 = _amount1;
             }
 
-            // @dev This is a sanity check to ensure that the amounts are not over the amounts specified
-            // This can occure because of rounding errors in mulDiv ceiling
-            if (amount0 > _amount0) revert Validly__deposit_insufficientToken0Deposited();
-            if (amount1 > _amount1) revert Validly__deposit_insufficientToken1Deposited();
+            // This is a sanity check to ensure that the amounts are not over the amounts specified.
+            // This can occur due to rounding errors in mulDiv ceiling.
+            if (amount0 > _amount0)
+                revert Validly__deposit_insufficientToken0Deposited();
+            if (amount1 > _amount1)
+                revert Validly__deposit_insufficientToken1Deposited();
         }
+
+        if (shares < _minShares) revert Validly__deposit_lessThanMinShares();
 
         if (shares == 0) revert Validly__deposit_zeroShares();
 
         _mint(_recipient, shares);
 
-        (amount0, amount1) = pool.depositLiquidity(amount0, amount1, msg.sender, "", abi.encode(msg.sender));
+        (amount0, amount1) = pool.depositLiquidity(
+            amount0,
+            amount1,
+            msg.sender,
+            "",
+            abi.encode(msg.sender)
+        );
     }
 
     /**
-     * @notice Withdraw liquidity from `POOL` and burn LP tokens.
+     * @notice Withdraw liquidity from `pool` and burn LP tokens.
      * @param _shares Amount of LP tokens to burn.
      * @param _amount0Min Minimum amount of token0 required for `_recipient`.
      * @param _amount1Min Minimum amount of token1 required for `_recipient`.
@@ -185,7 +213,13 @@ contract Validly is ISovereignALM, ERC20, ReentrancyGuard {
      * @return amount0 Amount of token0 withdrawn. WARNING: Potentially innacurate in case token0 is rebase.
      * @return amount1 Amount of token1 withdrawn. WARNING: Potentially innacurate in case token1 is rebase.
      */
-    function withdraw(uint256 _shares, uint256 _amount0Min, uint256 _amount1Min, uint256 _deadline, address _recipient)
+    function withdraw(
+        uint256 _shares,
+        uint256 _amount0Min,
+        uint256 _amount1Min,
+        uint256 _deadline,
+        address _recipient
+    )
         external
         ensureDeadline(_deadline)
         nonReentrant
@@ -193,7 +227,8 @@ contract Validly is ISovereignALM, ERC20, ReentrancyGuard {
     {
         if (_shares == 0) revert Validly__withdraw_zeroShares();
 
-        if (_recipient == address(0)) revert Validly__withdraw_invalidRecipient();
+        if (_recipient == address(0))
+            revert Validly__withdraw_invalidRecipient();
 
         (uint256 reserve0, uint256 reserve1) = pool.getReserves();
 
@@ -204,8 +239,10 @@ contract Validly is ISovereignALM, ERC20, ReentrancyGuard {
         if (amount0 == 0 || amount1 == 0) revert Validly__withdraw_AmountZero();
 
         // Slippage protection checks
-        if (amount0 < _amount0Min) revert Validly__withdraw_insufficientToken0Withdrawn();
-        if (amount1 < _amount1Min) revert Validly__withdraw_insufficientToken1Withdrawn();
+        if (amount0 < _amount0Min)
+            revert Validly__withdraw_insufficientToken0Withdrawn();
+        if (amount1 < _amount1Min)
+            revert Validly__withdraw_insufficientToken1Withdrawn();
 
         _burn(msg.sender, _shares);
 
@@ -213,58 +250,88 @@ contract Validly is ISovereignALM, ERC20, ReentrancyGuard {
     }
 
     /**
-     * @notice Callback to transfer tokens from user into `POOL` during deposits.
+     * @notice Callback to transfer tokens from user into `pool` during deposits.
      */
-    function onDepositLiquidityCallback(uint256 _amount0, uint256 _amount1, bytes memory _data)
-        external
-        override
-        onlyPool
-    {
+    function onDepositLiquidityCallback(
+        uint256 _amount0,
+        uint256 _amount1,
+        bytes memory _data
+    ) external override onlyPool {
         address user = abi.decode(_data, (address));
 
         if (_amount0 > 0) {
-            IERC20Metadata(pool.token0()).safeTransferFrom(user, msg.sender, _amount0);
+            IERC20Metadata(pool.token0()).safeTransferFrom(
+                user,
+                msg.sender,
+                _amount0
+            );
         }
 
         if (_amount1 > 0) {
-            IERC20Metadata(pool.token1()).safeTransferFrom(user, msg.sender, _amount1);
+            IERC20Metadata(pool.token1()).safeTransferFrom(
+                user,
+                msg.sender,
+                _amount1
+            );
         }
     }
 
     /**
-     * @notice Swap callback from POOL.
+     * @notice Swap callback from pool.
      * @param _poolInput Contains fundamental data about the swap.
      * @return quote Quote information that prices tokenIn and tokenOut.
      */
     function getLiquidityQuote(
         ALMLiquidityQuoteInput memory _poolInput,
-        bytes calldata, /*_externalContext*/
+        bytes calldata /*_externalContext*/,
         bytes calldata /*_verifierData*/
     ) external view override returns (ALMLiquidityQuote memory quote) {
-        if (_poolInput.feeInBips == 0) revert Validly__getLiquidityQuote_feeInBipsZero();
+        if (_poolInput.feeInBips == 0)
+            revert Validly__getLiquidityQuote_feeInBipsZero();
 
         (uint256 reserve0, uint256 reserve1) = pool.getReserves();
 
-        (uint256 reserveIn, uint256 reserveOut) = _poolInput.isZeroToOne ? (reserve0, reserve1) : (reserve1, reserve0);
+        (uint256 reserveIn, uint256 reserveOut) = _poolInput.isZeroToOne
+            ? (reserve0, reserve1)
+            : (reserve1, reserve0);
 
         if (isStable) {
             uint256 stableInvariant = _stableInvariant(reserve0, reserve1);
-            reserveIn = _poolInput.isZeroToOne ? reserveIn * 1e18 / decimals0 : reserveIn * 1e18 / decimals1;
-            reserveOut = _poolInput.isZeroToOne ? reserveOut * 1e18 / decimals1 : reserveOut * 1e18 / decimals0;
+            // Scale reserves and amounts to 18 decimals
+            reserveIn = _poolInput.isZeroToOne
+                ? (reserveIn * 1e18) / decimals0
+                : (reserveIn * 1e18) / decimals1;
+            reserveOut = _poolInput.isZeroToOne
+                ? (reserveOut * 1e18) / decimals1
+                : (reserveOut * 1e18) / decimals0;
             uint256 amountIn = _poolInput.isZeroToOne
-                ? _poolInput.amountInMinusFee * 1e18 / decimals0
-                : _poolInput.amountInMinusFee * 1e18 / decimals1;
-            uint256 y = reserveOut - _get_y(amountIn + reserveIn, stableInvariant, reserveOut);
-            quote.amountOut = y * (_poolInput.isZeroToOne ? decimals1 : decimals0) / 1e18;
+                ? (_poolInput.amountInMinusFee * 1e18) / decimals0
+                : (_poolInput.amountInMinusFee * 1e18) / decimals1;
+            uint256 amountOut = reserveOut -
+                _get_y_stableInvariant(
+                    amountIn + reserveIn,
+                    stableInvariant,
+                    reserveOut
+                );
+
+            quote.amountOut =
+                (amountOut * (_poolInput.isZeroToOne ? decimals1 : decimals0)) /
+                1e18;
         } else {
-            quote.amountOut = (reserveOut * _poolInput.amountInMinusFee) / (reserveIn + _poolInput.amountInMinusFee);
+            quote.amountOut =
+                (reserveOut * _poolInput.amountInMinusFee) /
+                (reserveIn + _poolInput.amountInMinusFee);
         }
 
         quote.amountInFilled = _poolInput.amountInMinusFee;
     }
 
     // solhint-disable-next-line no-empty-blocks
-    function onSwapCallback(bool, /*_isZeroToOne*/ uint256, /*_amountIn*/ uint256 /*_amountOut*/ ) external override {}
+    function onSwapCallback(
+        bool /*_isZeroToOne*/,
+        uint256 /*_amountIn*/,
+        uint256 /*_amountOut*/
+    ) external override {}
 
     /**
      *
@@ -277,23 +344,34 @@ contract Validly is ISovereignALM, ERC20, ReentrancyGuard {
         }
     }
 
-    function _f(uint256 x0, uint256 y) internal pure returns (uint256) {
-        return x0 * (y * y / 1e18 * y / 1e18) / 1e18 + (x0 * x0 / 1e18 * x0 / 1e18) * y / 1e18;
+    function _f(uint256 x0, uint256 y) private pure returns (uint256) {
+        return
+            (x0 * ((((y * y) / 1e18) * y) / 1e18)) /
+            1e18 +
+            (((((x0 * x0) / 1e18) * x0) / 1e18) * y) /
+            1e18;
     }
 
-    function _d(uint256 x0, uint256 y) internal pure returns (uint256) {
-        return 3 * x0 * (y * y / 1e18) / 1e18 + (x0 * x0 / 1e18 * x0 / 1e18);
+    function _d(uint256 x0, uint256 y) private pure returns (uint256) {
+        return
+            (3 * x0 * ((y * y) / 1e18)) /
+            1e18 +
+            ((((x0 * x0) / 1e18) * x0) / 1e18);
     }
 
-    function _get_y(uint256 x0, uint256 xy, uint256 y) internal pure returns (uint256) {
+    function _get_y_stableInvariant(
+        uint256 x0,
+        uint256 invariant,
+        uint256 y
+    ) private pure returns (uint256) {
         for (uint256 i = 0; i < 255; i++) {
             uint256 y_prev = y;
             uint256 k = _f(x0, y);
-            if (k < xy) {
-                uint256 dy = (xy - k) * 1e18 / _d(x0, y);
+            if (k < invariant) {
+                uint256 dy = ((invariant - k) * 1e18) / _d(x0, y);
                 y = y + dy;
             } else {
-                uint256 dy = (k - xy) * 1e18 / _d(x0, y);
+                uint256 dy = ((k - invariant) * 1e18) / _d(x0, y);
                 y = y - dy;
             }
             if (y > y_prev) {
@@ -306,14 +384,19 @@ contract Validly is ISovereignALM, ERC20, ReentrancyGuard {
                 }
             }
         }
-        return y;
+        // Did not converge in 256 fixed point iterations,
+        // revert for safety
+        revert Validly___get_y_notConverged();
     }
 
-    function _stableInvariant(uint256 x, uint256 y) internal view returns (uint256) {
-        uint256 _x = x * 1e18 / decimals0;
-        uint256 _y = y * 1e18 / decimals1;
+    function _stableInvariant(
+        uint256 x,
+        uint256 y
+    ) private view returns (uint256) {
+        uint256 _x = (x * 1e18) / decimals0;
+        uint256 _y = (y * 1e18) / decimals1;
         uint256 _a = (_x * _y) / 1e18;
         uint256 _b = ((_x * _x) / 1e18 + (_y * _y) / 1e18);
-        return _a * _b / 1e18; // x3y+y3x >= k
+        return (_a * _b) / 1e18; // x3y+y3x >= k
     }
 }
--- src/ValidlyFactory.sol
+++ src/ValidlyFactory.sol
@@ -16,9 +16,18 @@ contract ValidlyFactory {
      *  EVENTS
      */
     event FeesClaimed(address indexed pool);
-    event PoolCreated(address indexed pool, address indexed token0, address indexed token1, bool isStable);
+    event PoolCreated(
+        address indexed pool,
+        address indexed token0,
+        address indexed token1,
+        bool isStable
+    );
     event PoolManagerFeeBipsSet(address indexed pool, uint256 feeBips);
-    event TokenClaimed(address indexed token, address indexed recipient, uint256 amount);
+    event TokenClaimed(
+        address indexed token,
+        address indexed recipient,
+        uint256 amount
+    );
 
     /**
      *  ERRORS
@@ -35,14 +44,15 @@ contract ValidlyFactory {
      */
 
     /**
-     * @notice The protocol factory contract used for deploying pools
-     *  @dev This is set in the constructor and cannot be changed
+     * @notice The protocol factory contract used for deploying pools.
+     * @dev This is set in the constructor and cannot be changed.
      */
     IProtocolFactory public immutable protocolFactory;
 
     /**
-     * @notice The fee percentage for the Validly pool
-     *  @dev This is set in the constructor and cannot be changed
+     * @notice The fee percentage for the Validly pool.
+     * @dev This is set in the constructor and cannot be changed.
+     * @dev If new pool fee tiers are required, one can deploy new Validly factories.
      */
     uint256 public immutable feeBips;
 
@@ -51,7 +61,7 @@ contract ValidlyFactory {
      */
 
     /**
-     * @notice Mapping from pool keys to pool addresses
+     * @notice Mapping from pool keys to pool addresses.
      */
     mapping(bytes32 key => address pool) public pools;
 
@@ -85,17 +95,23 @@ contract ValidlyFactory {
      */
 
     /**
-     * @notice Deploys a new Validly pool for a given token pair
-     * @dev Tokens are sorted internally to ensure consistent pool keys
-     * @param _token0 The address of the first token in the pair
-     * @param _token1 The address of the second token in the pair
-     * @param _isStable Boolean indicating if the pool should be stable or volatile
-     * @custom:error ValidlyFactory__createPair_alreadyDeployed Thrown if a pool for the given token pair and stability type already exists
-     * @custom:error ValidlyFactory__createPair_failedDeployment Thrown if the Validly contract deployment fails
-     * @custom:error ValidlyFactory__createPair_invalidFeeBips Thrown if the feeBips is not between 0 and 10000
+     * @notice Deploys a new Validly pool for a given token pair.
+     * @dev Tokens are sorted internally to ensure consistent pool keys.
+     * @param _token0 The address of the first token in the pair.
+     * @param _token1 The address of the second token in the pair.
+     * @param _isStable Boolean indicating if the pool should be stable or volatile.
+     * @custom:error ValidlyFactory__createPair_alreadyDeployed Thrown if a pool for the given token pair and stability type already exists.
+     * @custom:error ValidlyFactory__createPair_failedDeployment Thrown if the Validly contract deployment fails.
+     * @custom:error ValidlyFactory__createPair_invalidFeeBips Thrown if the feeBips is not between 0 and 10000.
      */
-    function createPair(address _token0, address _token1, bool _isStable) external returns (address) {
-        (_token0, _token1) = _token0 < _token1 ? (_token0, _token1) : (_token1, _token0);
+    function createPair(
+        address _token0,
+        address _token1,
+        bool _isStable
+    ) external returns (address) {
+        (_token0, _token1) = _token0 < _token1
+            ? (_token0, _token1)
+            : (_token1, _token0);
 
         bytes32 poolKey = _poolKey(_token0, _token1, _isStable);
 
@@ -135,13 +151,16 @@ contract ValidlyFactory {
     }
 
     /**
-     * @notice Creates a new Validly pool, mostly for rebase tokens, which is not indexed in pools mapping
-     * @dev This function is used to create a pool given the SovereignPool constructor arguments
-     * @param _args The constructor arguments for the SovereignPool
-     * @param _isStable Boolean indicating if the pool should be stable or volatile
-     * @custom:error ValidlyFactory__createPool_failedDeployment Thrown if the Validly contract deployment fails
+     * @notice Creates a new Validly pool, mostly for rebase tokens, which is not indexed in pools mapping.
+     * @dev This function is used to create a pool given the SovereignPool constructor arguments.
+     * @param _args The constructor arguments for the SovereignPool.
+     * @param _isStable Boolean indicating if the pool should be stable or volatile.
+     * @custom:error ValidlyFactory__createPool_failedDeployment Thrown if the Validly contract deployment fails.
      */
-    function createPool(SovereignPoolConstructorArgs memory _args, bool _isStable) external returns (address validly) {
+    function createPool(
+        SovereignPoolConstructorArgs memory _args,
+        bool _isStable
+    ) external returns (address validly) {
         _args.poolManager = address(this);
 
         address pool = protocolFactory.deploySovereignPool(_args);
@@ -158,35 +177,46 @@ contract ValidlyFactory {
     }
 
     /**
-     * @notice Sets the pool manager fees for a given pool
-     * @dev This function is used to set the pool manager fees for a given pool
-     * @param _pool The address of the pool to set the pool manager fees for
-     * @param _feeBips The fee percentage for the pool manager
-     * @custom:error ValidlyFactory__setPoolManagerFees_unauthorized Thrown if the caller is not the protocol manager
+     * @notice Sets the pool manager fees for a given pool.
+     * @dev This function is used to set the pool manager fees for a given pool.
+     * @param _pool The address of the pool to set the pool manager fees for.
+     * @param _feeBips The fee percentage for the pool manager.
+     * @custom:error ValidlyFactory__setPoolManagerFees_unauthorized Thrown if the caller is not the protocol manager.
      */
-    function setPoolManagerFeeBips(address _pool, uint256 _feeBips) external onlyProtocolManager {
+    function setPoolManagerFeeBips(
+        address _pool,
+        uint256 _feeBips
+    ) external onlyProtocolManager {
         ISovereignPool(_pool).setPoolManagerFeeBips(_feeBips);
 
         emit PoolManagerFeeBipsSet(_pool, _feeBips);
     }
 
     /**
-     * @notice Claims rebase token fees accumulated in Factory
-     * @param _token The address of the token to claim
-     * @param _recipient The address of the recipient
+     * @notice Claims rebase token fees accumulated in this contract.
+     * @dev By design of Sovereign Pools, manager fees for rebase tokens
+     *      get transferred on every swap to its manager (this contract).
+     * @param _token The address of the token to claim.
+     * @param _recipient The address of the recipient.
      */
-    function claimTokens(address _token, address _recipient) external onlyProtocolManager {
+    function claimTokens(
+        address _token,
+        address _recipient
+    ) external onlyProtocolManager {
         IERC20 token = IERC20(_token);
         uint256 balance = token.balanceOf(address(this));
-        token.safeTransfer(_recipient, balance);
 
-        emit TokenClaimed(_token, _recipient, balance);
+        if (balance > 0) {
+            token.safeTransfer(_recipient, balance);
+
+            emit TokenClaimed(_token, _recipient, balance);
+        }
     }
 
     /**
-     * @notice Claims the pool manager fees for a given pool
-     * @dev This function is used to claim the pool manager fees for a given pool
-     * @param _pool The address of the pool to claim the pool manager fees for
+     * @notice Claims the pool manager fees for a given pool.
+     * @dev This function is used to claim the pool manager fees for a given pool.
+     * @param _pool The address of the pool to claim the pool manager fees for.
      */
     function claimFees(address _pool) external {
         // It marks all fees as protocol fees to be used by gauge
@@ -196,9 +226,13 @@ contract ValidlyFactory {
     }
 
     /**
-     *  INTERNAL FUNCTIONS
+     *  PRIVATE FUNCTIONS
      */
-    function _poolKey(address token0, address token1, bool isStable) internal pure returns (bytes32 key) {
+    function _poolKey(
+        address token0,
+        address token1,
+        bool isStable
+    ) private pure returns (bytes32 key) {
         key = keccak256(abi.encode(token0, token1, isStable));
     }
 }
